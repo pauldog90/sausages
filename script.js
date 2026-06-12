3c23986f35a2e4852ee8c79113cf2b6e4e1f93f6
paulwhite5@outlook.com <paulwhite5@outlook.com>
2026-06-12 17:06:37 +0100

    new comic 0k

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // List of comics
 document.addEventListener('DOMContentLoaded', () => {
   const comics = [
+    "Images/Comics/260409-0k.jpg",
     "Images/Comics/260511-sale.jpg",
     "Images/Comics/260503-hats.jpg",
     "Images/Comics/260510-sillycone.jpg",
